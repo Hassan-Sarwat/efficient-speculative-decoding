a91e6f9678163f47d1d9f57ab676eaba06c1d2d7
Hassan Sarwat <hassansarwat96@gmail.com>
2026-04-08 16:49:39 +0200

    updates

--- scripts/run_queue.sh
+++ scripts/run_queue.sh
@@ -11,6 +11,9 @@ bash scripts/train_pipeline.sh -t cod -s easy
 echo "👉 Running: CoT - Easy"
 bash scripts/train_pipeline.sh -t cot -s easy
 
+echo "👉 Running: Untrained - Easy"
+bash scripts/untrained_pipeline.sh easy
+
 # 2. Medium Scenarios
 echo "👉 Running: CoD - Medium"
 bash scripts/train_pipeline.sh -t cod -s medium
@@ -18,12 +21,18 @@ bash scripts/train_pipeline.sh -t cod -s medium
 echo "👉 Running: CoT - Medium"
 bash scripts/train_pipeline.sh -t cot -s medium
 
+echo "👉 Running: Untrained - Medium"
+bash scripts/untrained_pipeline.sh medium
+
 # 3. Hard Scenarios
 echo "👉 Running: CoD - Hard"
 bash scripts/train_pipeline.sh -t cod -s hard
 
 echo "👉 Running: CoT - Hard"
 bash scripts/train_pipeline.sh -t cot -s hard
+
+echo "👉 Running: Untrained - Hard"
+bash scripts/untrained_pipeline.sh hard
 
 end_time=$(date)
 echo "✅ All jobs completed successfully!"
